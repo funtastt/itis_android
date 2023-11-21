b38ceb878e6b5676efe5f6e6385b2b8fad81bb28
Salavat <asnsalavat@gmail.com>
2023-11-21 11:08:50 +0300

    Updated app default structure

--- app/src/main/java/ru/kpfu/itis/android/asadullin/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/android/asadullin/MainActivity.kt
@@ -1,16 +1,21 @@
 package ru.kpfu.itis.android.asadullin
 
-import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainerView
-import ru.kpfu.itis.android.asadullin.lesson1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment_container)
+    }
+
+    fun replaceFragment(fragment: Fragment, tag: String, addToBackStack: Boolean) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment, tag)
+        if (addToBackStack) {
+            transaction.addToBackStack(null)
+        }
+        transaction.commit()
     }
 }
